2c70100499a52ee9fc4a17112e6a9b19f954b510
Carlos Oliveira <carlos.eduardo02@gmail.com>
2018-09-20 13:54:12 -0300

    Playing with routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ola', function () {
+    return '<h1>Seja bem vindo</h1>';
+});
+
+Route::get('/ola/{nome}', function ($nome) {
+    return "<h1>Olá, $nome</h1>";
+});
+
+Route::get('/ola/{nome}/{sobnome}', function ($nome, $sobnome) {
+    return "<h1>Olá, $nome $sobnome</h1>";
+});
+
+Route::get('/repetir/{nome}/{num}', function($nome, $num) {
+    for($i = 0; $i < $num; $i++) {
+        echo "<h1>$nome</h1>";
+    }
+});
