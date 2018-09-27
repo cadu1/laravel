5a96e5ce57aa997fe8b38322c7654bf806d4b851
Carlos Oliveira <carlos.eduardo02@gmail.com>
2018-09-27 15:51:03 -0300

    First steps with soft delete

--- database/migrations/2018_09_25_185452_criar_tabela_categoria.php
+++ database/migrations/2018_09_25_185452_criar_tabela_categoria.php
@@ -16,6 +16,7 @@ class CriarTabelaCategoria extends Migration
         Schema::create('categorias', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nomes');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
